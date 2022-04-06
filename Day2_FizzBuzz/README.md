bf68de32a50b2dabd8000937262854e9d4cbbe3a
Abdulkareem Mustapha <aphatheology@gmail.com>
2022-04-06 22:39:03 +0100

    Day 1 - FizzBuzz

---
+++ Day2_FizzBuzz/README.md
@@ -0,0 +1,9 @@
+# Task: Write a function that that takes an integer n as its input. The function should print all numbers from 1 to n but follow these rules as well:
+
+- If the number is divisible by 3, print Fizz in its place
+
+- If the number is divisible by 5, print Buzz in its place
+
+- If divisible by both 3 and 5, print FizzBuzz
+
+## Hosted link: https://aphatheology.github.io/30DaysOfCodeR2/Day2_Fizz_Buzz/
---
+++ Day2_FizzBuzz/script.js
@@ -0,0 +1,20 @@
+function fizzBuzz(n) {
+    // First Idea
+    let output = ""
+    for(let i = 1; i <= n; i++) {
+        if(i % 3 === 0 && i % 5 != 0 ) {
+            output += `Fizz `;
+        } else if(i % 3 != 0 && i % 5 === 0 ) {
+            output += `Buzz `;
+        } else if(i % 3 === 0 && i % 5 === 0 ) {
+            output += `FizzBuzz `;
+        } else {
+            output += `${i} `;
+        }
+        
+    }
+    console.log(output)
+}
+
+// TEST
+fizzBuzz(23)
