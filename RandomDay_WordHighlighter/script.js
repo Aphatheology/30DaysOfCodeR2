828cfc3e1561eadb485fee6077dca3d1cdf126d5
Abdulkareem Mustapha <aphatheology@gmail.com>
2022-05-03 00:13:00 +0100

    Implement word limit for each input

--- RandomDay_WordHighlighter/script.js
+++ RandomDay_WordHighlighter/script.js
@@ -2,7 +2,7 @@ const allText = document.querySelector('.all-text');
 const mainText = document.querySelector('.main-text');
 const button = document.querySelector('button');
 const header = document.querySelector('h2')
-
+const wordLimit = document.querySelectorAll('input[data-max-words]');
 button.addEventListener('click', highlightWord);
 
 function highlightWord() {
@@ -34,3 +34,31 @@ function highlightWord() {
 	}
     // console.log(new Date().getTime() - start);
 }
+
+// function wordLimit(inp, limit){
+//     let val = inp.value
+//     let words = val.split(/\s+/);
+//     let legal = "";
+//     for(i = 0; i < words.length; i++) {
+//         if(i < limit) {
+//             legal += words[i] + " ";
+//         }
+//         if(i >= limit) {
+//             inp.value = legal.trim();
+//             // mainText.disabled = true;
+//         }
+//         // e.keyCode == 32 && (e.preventDefault() || alert('Word Limit Reached')
+//     }
+// }
+
+wordLimit.forEach(input => {
+    let maxWords = parseInt(input.getAttribute('data-max-words') || 0)
+    input.addEventListener('keydown', e => {
+      let target = e.currentTarget
+      let words = target.value.split(/\s+/).length
+      if (!target.getAttribute('data-announce'))
+        words >= maxWords && e.keyCode == 32 && e.preventDefault()
+      else
+        words >= maxWords && e.keyCode == 32 && (e.preventDefault() || alert(`Only ${maxWords} words allowed`))
+    })
+  })
